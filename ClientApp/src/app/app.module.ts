760becc1fb35ede6859f3a8a1eeb0edef9a2b058
Neal Robben <neal.robben@gmail.com>
2020-08-11 21:55:42 +0200

    Added Angular model

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,14 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ModelModule } from "./models/model.module";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule,
+    AppRoutingModule,
+    ModelModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ ClientApp/src/app/models/repository.ts
@@ -0,0 +1,8 @@
+import { Product } from "./product.model";
+
+export class Repository {
+  constructor() {
+    this.product = JSON.parse(document.getElementById("data").textContent);
+  }
+  product: Product;
+}
