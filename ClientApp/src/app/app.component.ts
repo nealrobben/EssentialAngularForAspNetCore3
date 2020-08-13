2e850fe52aa348c351a1227fc19d0573e4388e2b
Neal Robben <neal.robben@gmail.com>
2020-08-13 21:54:42 +0200

    Implemented updating of Products/Suppliers

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -26,11 +26,24 @@ export class AppComponent {
     this.repo.createProduct(new Product(0, "X-Ray Scuba Mask", "Watersports",
       "See what the fish are hiding", 49.99, this.repo.products[0].supplier));
   }
+
   createProductAndSupplier() {
     let s = new Supplier(0, "Rocket Shoe Corp", "Boston", "MA");
     let p = new Product(0, "Rocket-Powered Shoes", "Running",
       "Set a new record", 100, s);
     this.repo.createProductAndSupplier(p, s);
+  }
+
+  replaceProduct() {
+    let p = this.repo.products[0];
+    p.name = "Modified Product";
+    p.category = "Modified Category";
+    this.repo.replaceProduct(p);
+  }
+
+  replaceSupplier() {
+    let s = new Supplier(3, "Modified Supplier", "New York", "NY");
+    this.repo.replaceSupplier(s);
   }
 
 }
