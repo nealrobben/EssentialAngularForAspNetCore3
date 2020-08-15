b12fbb773ec68885b664ac3bf7a39800b1f8d26d
Neal Robben <neal.robben@gmail.com>
2020-08-15 13:46:54 +0200

    Added routing

--- ClientApp/src/app/structure/productDetail.component.ts
+++ ClientApp/src/app/structure/productDetail.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Repository } from "../models/repository";
 import { Product } from "../models/product.model";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "product-detail",
@@ -8,7 +9,16 @@ import { Product } from "../models/product.model";
 })
 export class ProductDetailComponent {
 
-  constructor(private repo: Repository) { }
+  constructor(private repo: Repository,
+    router: Router,
+    activeRoute: ActivatedRoute) {
+    let id = Number.parseInt(activeRoute.snapshot.params["id"]);
+    if (id) {
+      this.repo.getProduct(id);
+    } else {
+      router.navigateByUrl("/");
+    }
+  }
 
   get product(): Product {
     return this.repo.product;
--- ClientApp/src/app/structure/productTable.component.ts
+++ ClientApp/src/app/structure/productTable.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Repository } from "../models/repository";
 import { Product } from "../models/product.model";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "product-table",
@@ -8,7 +9,8 @@ import { Product } from "../models/product.model";
 })
 export class ProductTableComponent {
 
-  constructor(private repo: Repository) { }
+  constructor(private repo: Repository,
+    private router: Router) { }
 
   get products(): Product[] {
     return this.repo.products;
@@ -16,6 +18,7 @@ export class ProductTableComponent {
 
   selectProduct(id: number) {
     this.repo.getProduct(id);
+    this.router.navigateByUrl("/detail");
   }
 
 }
