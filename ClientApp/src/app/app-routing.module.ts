5693c54a27588cf15a0f9195bbe6c3b39f4ccede
Neal Robben <neal.robben@gmail.com>
2020-08-18 21:59:25 +0200

    Added basic administration

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -12,6 +12,11 @@ import { OrderConfirmationComponent }
   from "./store/checkout/orderConfirmation.component";
 
 const routes: Routes = [
+  {
+    path: "admin",
+    loadChildren: () =>
+      import("./admin/admin.module").then(module => module.AdminModule),
+  },
   { path: "checkout/step1", component: CheckoutDetailsComponent },
   { path: "checkout/step2", component: CheckoutPaymentComponent },
   { path: "checkout/step3", component: CheckoutSummaryComponent },
