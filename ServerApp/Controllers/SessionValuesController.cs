ed158b7e749a5612896d6e1f7d011a63dd52daa0
Neal Robben <neal.robben@gmail.com>
2020-08-18 21:15:47 +0200

    Store checkout data in session

--- ServerApp/Controllers/SessionValuesController.cs
+++ ServerApp/Controllers/SessionValuesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using ServerApp.Models;
+using ServerApp.Models.BindingTargets;
 
 namespace ServerApp.Controllers
 {
@@ -14,11 +15,25 @@ namespace ServerApp.Controllers
         {
             return Ok(HttpContext.Session.GetString("cart"));
         }
+
         [HttpPost("cart")]
         public void StoreCart([FromBody] ProductSelection[] products)
         {
             var jsonData = JsonConvert.SerializeObject(products);
             HttpContext.Session.SetString("cart", jsonData);
+        }
+
+        [HttpGet("checkout")]
+        public IActionResult GetCheckout()
+        {
+            return Ok(HttpContext.Session.GetString("checkout"));
+        }
+
+        [HttpPost("checkout")]
+        public void StoreCheckout([FromBody] CheckoutState data)
+        {
+            HttpContext.Session.SetString("checkout",
+            JsonConvert.SerializeObject(data));
         }
     }
 }
