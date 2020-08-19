1c8ecb4bbe7ed8fa1983f46642b3b8c6f64f7641
Neal Robben <neal.robben@gmail.com>
2020-08-19 23:04:32 +0200

    Added login in angular

--- ClientApp/src/app/admin/admin.module.ts
+++ ClientApp/src/app/admin/admin.module.ts
@@ -7,10 +7,15 @@ import { ProductAdminComponent } from "./productAdmin.component";
 import { OrderAdminComponent } from "./orderAdmin.component";
 import { ProductEditorComponent } from "./productEditor.component";
 import { CommonModule } from '@angular/common';
+import { AuthModule } from '../auth/auth.module';
+import { AuthenticationComponent } from '../auth/authentication.component';
+import { AuthenticationGuard } from '../auth/authentication.guard';
 
 const routes: Routes = [
+  { path: "login", component: AuthenticationComponent },
   {
     path: "", component: AdminComponent,
+    canActivateChild: [AuthenticationGuard],
     children: [
       { path: "products", component: ProductAdminComponent },
       { path: "orders", component: OrderAdminComponent },
@@ -20,7 +25,7 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [RouterModule,
-    FormsModule, RouterModule.forChild(routes), CommonModule],
+    FormsModule, RouterModule.forChild(routes), CommonModule, AuthModule],
   declarations: [AdminComponent, OverviewComponent,
     ProductAdminComponent, OrderAdminComponent, ProductEditorComponent]
 })
